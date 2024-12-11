48b68e424c9a1bb73022197c5e1a0c7aab30d9cd
cectpehka <151868333+CECTPEHKA@users.noreply.github.com>
2024-12-11 11:02:06 +0200

    practice4

---
+++ WebApplication1/WebApplication1/Data/DataContext .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Models;
+
+namespace ProductCatalogAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; } // Таблиця для товарів
+    }
+}
